0b0c054bb50a4a311798ce4e276495921e48e97e
FIN-theFirst <fredericneumann@outlook.de>
2025-07-31 13:04:20 +0200

    Added new clothingProfile files for new Data structure and process data

---
+++ services/clothingProfile.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const { clothingProfileTypes } = require("../services/clothingProfileTypes");
+
+async function fetchClothingProfile(userId) {
+  const url = '${process.env.PROFILE_API_URL}/${userId}';
+  const res = await fetch(url, {
+    headers: {
+      Authorization: 'Bearer ${process.env.PROFILE_API_URL}'
+    }
+  })
+  if (!res.ok) {
+    throw new Error('Error while fetching: ${res.statusText}'); kK: w
+  }
+  const data = await res.json();
+  return data;
+}
+module.exports = { fetchClothingProfile };
+
---
+++ services/clothingProfileTypes.js
@@ -0,0 +1,21 @@
+// Klamotten in Kategorien Unterteilen
+// Schuhe
+// Hose
+// Shirts
+// Jacken
+class clothingProfileTypes {
+  constructor(data) {
+    this.shoes = {
+
+    };
+    this.pants = {
+
+    };
+    this.shirts = {
+
+    };
+    this.extras = {
+
+    }
+  }
+}
